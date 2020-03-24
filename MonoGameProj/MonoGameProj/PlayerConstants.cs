bcb196c85bc8729f08a0a6a119aa6abd7751d2b4
leederJosh <leederJosh>
2020-03-24 15:47:56 +0000

    Added player constants class for future use

---
+++ MonoGameProj/MonoGameProj/PlayerConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGameProj
+{
+    public static class PlayerConstants
+    {
+        static readonly int PLAYER_MOVE_PER_FRAME = 5;
+    }
+}
