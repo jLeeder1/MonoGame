6adaebc251a99e4b32d42846ebe6412ed5b7f8ec
leederJosh <leederJosh>
2020-03-29 14:06:46 +0100

    Push broken dependecny injection t remote for pull down on another machine

--- MonoGameProj/MonoGameProj/Managers/GameSetupManager.cs
+++ MonoGameProj/MonoGameProj/Managers/GameSetupManager.cs
@@ -17,6 +17,7 @@ namespace MonoGameProj.Managers
             // Setup
             SetUpPlayerControls(players);
             PlayerMovementManager = new PlayerMovementManager(players, IMyKeyboardInput test);
+            // Why not injkect the movementManager and have this class as more of a mapper
         }
 
         private void SetUpPlayerControls(List<Player> players)
@@ -24,6 +25,8 @@ namespace MonoGameProj.Managers
             this.playerKeyAssociationManager.AssociateKeysWithPlayers(players);
         }
 
+        // think this is code smell too, so this class is supposed to set things up
+        // maybe it should set things up and then return them from am ethod rather thasn keeping hold of them in properties?
         public PlayerMovementManager PlayerMovementManager { get; private set; }
 
     }
